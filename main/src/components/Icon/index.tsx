8ce8056ba5bd7e38eafd3f84b0c63c46d7e797cb
Lin <1537053400@qq.com>
2022-04-10 16:00:37 +0800

    feat:echarts响应式+卡片筛选

--- main/src/components/Icon/index.tsx
+++ main/src/components/Icon/index.tsx
@@ -10,36 +10,42 @@ interface IProps {
 export const CalendarIcon = (props: IProps) => {
   const { active = false } = props
   const color = active ? '#FFF' : '#8BC6EC'
-  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01"/><rect x="4" y="8" width="40" height="36" fill="none" stroke={color} strokeWidth="3" strokeLinejoin="round"/><path fillRule="evenodd" clipRule="evenodd" d="M28 20V34H36V20H28Z" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M17 4V12" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M31 4V12" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 20H20V34H12" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M20 27H14" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/></svg>)
+  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01" /><rect x="4" y="8" width="40" height="36" fill="none" stroke={color} strokeWidth="3" strokeLinejoin="round" /><path fillRule="evenodd" clipRule="evenodd" d="M28 20V34H36V20H28Z" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /><path d="M17 4V12" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /><path d="M31 4V12" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /><path d="M12 20H20V34H12" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /><path d="M20 27H14" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /></svg>)
 }
 
 export const DoneIcon = (props: IProps) => {
   const { active = false } = props
   const color = active ? '#FFF' : '#8BC6EC'
-  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01"/><path d="M24 4L29.2533 7.83204L35.7557 7.81966L37.7533 14.0077L43.0211 17.8197L41 24L43.0211 30.1803L37.7533 33.9923L35.7557 40.1803L29.2533 40.168L24 44L18.7467 40.168L12.2443 40.1803L10.2467 33.9923L4.97887 30.1803L7 24L4.97887 17.8197L10.2467 14.0077L12.2443 7.81966L18.7467 7.83204L24 4Z" fill="none" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M17 24L22 29L32 19" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/></svg>)
+  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01" /><path d="M24 4L29.2533 7.83204L35.7557 7.81966L37.7533 14.0077L43.0211 17.8197L41 24L43.0211 30.1803L37.7533 33.9923L35.7557 40.1803L29.2533 40.168L24 44L18.7467 40.168L12.2443 40.1803L10.2467 33.9923L4.97887 30.1803L7 24L4.97887 17.8197L10.2467 14.0077L12.2443 7.81966L18.7467 7.83204L24 4Z" fill="none" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /><path d="M17 24L22 29L32 19" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /></svg>)
 }
 
 export const DoingIcon = (props: IProps) => {
   const { active = false } = props
   const color = active ? '#FFF' : '#8BC6EC'
-  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01"/><path d="M19.4545 26.4444C17.6364 28.2222 15.8182 30 12.1818 30C8.54545 30 4 27.3333 4 22C4 16.6667 8.54545 14 12.1818 14C17.6364 14 20.3636 17.5556 24 22C27.6364 26.4444 30.3636 30 35.8182 30C39.4545 30 44 27.3333 44 22C44 16.6667 39.4545 14 35.8182 14C32.1818 14 29.4545 16.6667 28.5455 17.5556" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/></svg>)
+  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01" /><path d="M19.4545 26.4444C17.6364 28.2222 15.8182 30 12.1818 30C8.54545 30 4 27.3333 4 22C4 16.6667 8.54545 14 12.1818 14C17.6364 14 20.3636 17.5556 24 22C27.6364 26.4444 30.3636 30 35.8182 30C39.4545 30 44 27.3333 44 22C44 16.6667 39.4545 14 35.8182 14C32.1818 14 29.4545 16.6667 28.5455 17.5556" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /></svg>)
 }
 
 export const ChartIcon = (props: IProps) => {
-    const { active = false } = props
-    const color = active ? '#FFF' : '#8BC6EC'
-    return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 24C5 34.4934 13.5066 43 24 43V26C24 24.8954 24.8954 24 26 24H43C43 13.5066 34.4934 5 24 5C13.5066 5 5 13.5066 5 24Z" fill="none" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><rect x="30" y="30" width="12" height="12" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/></svg>)
-  }
+  const { active = false } = props
+  const color = active ? '#FFF' : '#8BC6EC'
+  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 24C5 34.4934 13.5066 43 24 43V26C24 24.8954 24.8954 24 26 24H43C43 13.5066 34.4934 5 24 5C13.5066 5 5 13.5066 5 24Z" fill="none" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><rect x="30" y="30" width="12" height="12" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /></svg>)
+}
 
-  
-  export const SortIcon = (props: IProps) => {
-    const { active = false } = props
-    const color = active ? '#FFF' : '#8BC6EC'
-    return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01"/><path d="M19 6L19 42" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><path d="M7 17.8995L19 5.89949" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><path d="M29 42.1005L29 6.10051" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><path d="M29 42.1005L41 30.1005" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/></svg>)
-  }
 
-  export const PlusIcon = (props: IProps) =>{
-    const { active = false } = props
-    const color = active ? '#FFF' : '#8BC6EC'
-     return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01"/><path d="M24.0607 10L24.024 38" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><path d="M10 24L38 24" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/></svg>)
-  }
+export const SortIcon = (props: IProps) => {
+  const { active = false } = props
+  const color = active ? '#FFF' : '#8BC6EC'
+  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01" /><path d="M19 6L19 42" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><path d="M7 17.8995L19 5.89949" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><path d="M29 42.1005L29 6.10051" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><path d="M29 42.1005L41 30.1005" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /></svg>)
+}
+
+export const PlusIcon = (props: IProps) => {
+  const { active = false } = props
+  const color = active ? '#FFF' : '#8BC6EC'
+  return (<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fillOpacity="0.01" /><path d="M24.0607 10L24.024 38" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><path d="M10 24L38 24" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /></svg>)
+}
+
+export const FilterIcon = (props: IProps) => {
+  const { active = false } = props
+  const color = active ? '#FFF' : '#8BC6EC'
+  return (<svg width="24" height="24" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><g><g><rect fillOpacity="0.01" fill="#FFFFFF" x="0" y="0" width="48" height="48" strokeWidth="4" stroke="none" fillRule="evenodd" /><polygon stroke={color} strokeWidth="4" fill="none" fillRule="nonzero" strokeLinejoin="round" points="6 9 20.4 25.8177778 20.4 38.4444444 27.6 42 27.6 25.8177778 42 9" /></g></g></svg>)
+}
--- main/src/pages/components/TaskStatistics/index.tsx
+++ main/src/pages/components/TaskStatistics/index.tsx
@@ -6,39 +6,62 @@ import { API_RESULT } from '@/const';
 
 import * as ec from 'echarts';
 import { useEffect, useMemo, useRef, useState } from 'react';
-import { getConfig ,getGaugeConfig} from './config';
+import { getConfig, getGaugeConfig } from './config';
 // @ts-nocheck  
 import GridLayout from "react-grid-layout";
 
 import moment from 'moment';
+import { DownOutlined } from '@ant-design/icons';
+
 
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
 import './index.less';
 import { getLocal, saveLocal } from '@/utils';
+import { Button, Checkbox, Dropdown, Menu, Popover } from 'antd';
+import MenuItem from 'antd/lib/menu/MenuItem';
+import { FilterIcon } from '@/components/Icon';
 
 interface IProps { }
 
-const DEFAULT_LAYOUT= [
-  { i: "allRemain", x: 2, y: 1, w: 4, h: 3,minW: 4, minH: 3 },
-  { i: "todaysRemain", x: 0, y: 0, w: 4, h: 3,minW: 4, minH: 3 },
-  { i: "todaysFinished", x: 1, y: 0, w: 4, h: 3,minW: 4, minH: 3 },
-  { i: "allFinished", x: 2, y: 1, w: 4, h: 3,minW: 4, minH: 3 },
-  { i: "outdatedCount", x: 3, y: 1, w: 4, h: 3,minW: 4, minH: 3 },
+const DEFAULT_LAYOUT = [
+  { i: "allRemain", x: 2, y: 1, w: 4, h: 3, minW: 4, minH: 3 },
+  { i: "todaysRemain", x: 0, y: 0, w: 4, h: 3, minW: 4, minH: 3 },
+  { i: "todaysFinished", x: 1, y: 0, w: 4, h: 3, minW: 4, minH: 3 },
+  { i: "allFinished", x: 2, y: 1, w: 4, h: 3, minW: 4, minH: 3 },
+  { i: "outdatedCount", x: 3, y: 1, w: 4, h: 3, minW: 4, minH: 3 },
 
   { i: "finishedRadio", x: 4, y: 0, w: 5, h: 6, minW: 4, minH: 6 },
-  { i: "finishedProgress", x: 4, y: 0, w: 6, h: 7,minW: 6, minH: 7  },
+  { i: "finishedProgress", x: 4, y: 0, w: 6, h: 7, minW: 6, minH: 7 },
 
 ];
 
-const LAYOUT_LOCAL_KEY='todo-layout'
+const DEFAULT_CARDS = [
+  { i: "allRemain", title: '总剩余任务量' },
+  { i: "todaysRemain", title: '今日截止任务量' },
+  { i: "todaysFinished", title: '今日已完成任务量' },
+  { i: "allFinished", title: '累计已完成任务量' },
+  { i: "outdatedCount", title: '已截止任务量' },
+
+  { i: "finishedRadio", title: '任务完成比' },
+  { i: "finishedProgress", title: '任务完成进度' },
+
+];
+
+const LAYOUT_LOCAL_KEY = 'todo-layout'
+const CARDS_LOCAL_KEY = 'todo-cards'
+// const DEFAULT_CARD_KEYS = DEFAULT_LAYOUT.map(i => i.i)
 
 export default function TaskStatistics(props: IProps) {
   const [allData, setAllData] = useState<{ doing: any[]; done: any[] }>();
-  const [layout,setLayout]=useState<GridLayout.Layout[]>(getLocal(LAYOUT_LOCAL_KEY,DEFAULT_LAYOUT))
-
-  const chartRefs = useRef<Record<string,ec.EChartsType>>({});
+  const [layout, setLayout] = useState<GridLayout.Layout[]>(
+    getLocal(LAYOUT_LOCAL_KEY, DEFAULT_LAYOUT)
+  )
+  const [visibleCards, setVisibleCards] = useState<string[]>(
+    getLocal(CARDS_LOCAL_KEY, DEFAULT_CARDS)
+  );
+  const chartRefs = useRef<Record<string, ec.EChartsType>>({});
 
   useEffect(() => {
     api(apiConfig.all.url).then((res) => {
@@ -52,19 +75,19 @@ export default function TaskStatistics(props: IProps) {
   const todaysRemain = useMemo(() => {
     const today = new Date().toISOString().split('T')[0];
     return allData?.doing.filter((i) =>
-    moment(i.endTime.split('T')[0]).isSame(moment(today)),
-  ).length;
+      moment(i.endTime.split('T')[0]).isSame(moment(today)),
+    ).length;
   }, [allData]);
 
   const outdatedCount = useMemo(() => {
     const today = new Date().toISOString().split('T')[0];
     return allData?.doing.filter((i) =>
-    moment(i.endTime.split('T')[0]).isBefore(moment(today)),
-  ).length;
+      moment(i.endTime.split('T')[0]).isBefore(moment(today)),
+    ).length;
   }, [allData]);
 
   const allRemain = useMemo(() => {
-      return allData?.doing.length;
+    return allData?.doing.length;
   }, [allData]);
 
   const todaysFinished = useMemo(() => {
@@ -79,21 +102,21 @@ export default function TaskStatistics(props: IProps) {
   }, [allData]);
 
   useEffect(() => {
-    const ids=['task-chart','task-guage-chart']
+    const ids = ['task-chart', 'task-guage-chart']
     const radioDom = document.getElementById('task-chart');
-    ids.forEach(i=>{
-      const dom =document.getElementById(i)
-      if(dom && chartRefs.current){
-        const chart=ec.init(dom);
-        chartRefs.current[i]=chart
+    ids.forEach(i => {
+      const dom = document.getElementById(i)
+      if (dom && chartRefs.current) {
+        const chart = ec.init(dom);
+        chartRefs.current[i] = chart
       }
     })
   }, []);
 
   useEffect(() => {
-    if(allData){
-      if(chartRefs.current?.['task-chart']){
-        const chartObj=chartRefs.current['task-chart']
+    if (allData) {
+      if (chartRefs.current?.['task-chart']) {
+        const chartObj = chartRefs.current['task-chart']
         const dataSource = [
           {
             name: '进行中',
@@ -107,77 +130,103 @@ export default function TaskStatistics(props: IProps) {
         const option = getConfig(dataSource);
         chartObj.setOption(option);
       }
-      if(chartRefs.current?.['task-guage-chart']&& todaysRemain!==undefined && todaysFinished!==undefined){
-        const chartObj=chartRefs.current['task-guage-chart']
-        const prop=todaysFinished / (todaysRemain+todaysFinished)*100
-        const option = getGaugeConfig(isNaN(prop)?0:prop);
+      if (chartRefs.current?.['task-guage-chart'] && todaysRemain !== undefined && todaysFinished !== undefined) {
+        const chartObj = chartRefs.current['task-guage-chart']
+        const prop = todaysFinished / (todaysRemain + todaysFinished) * 100
+        const option = getGaugeConfig(isNaN(prop) ? 0 : prop);
         chartObj.setOption(option);
       }
     }
 
   }, [allData]);
 
-
-  const handleLayoutChange=(l: GridLayout.Layout[])=>{
-    saveLocal(LAYOUT_LOCAL_KEY,l)
+  const handleCardsChange = (checkedValue: any[]) => {
+    setVisibleCards(checkedValue)
+    saveLocal(CARDS_LOCAL_KEY,checkedValue)
+    // console.log(checkedValue,'143')
   }
 
-  const renderCards=()=>{
-    const cards=[
+  // onChange={(e) => {
+  //   if (e.target.checked) {
+  //     setVisibleCards([...visibleCards, i])
+  //   }
+  //   else {
+  //     setVisibleCards([...visibleCards.filter(c => c.i !== i.i)])
+  //   }
+  // }}
+  const menu = (
+    <Checkbox.Group 
+    options={DEFAULT_CARDS.map(i=>({value:i.i,label:i.title}))} 
+    value={visibleCards} 
+    style={{display:'flex',flexDirection:'column'}}
+    onChange={handleCardsChange}>
+
+    </Checkbox.Group>
+  );
+  const handleLayoutChange = (l: GridLayout.Layout[]) => {
+    Object.values(chartRefs.current).forEach(c=>{
+      c.resize()
+    })
+    
+    saveLocal(LAYOUT_LOCAL_KEY, l)
+  }
+
+  const renderCards = () => {
+    const cards = [
       {
-        theme:'blue',
-        title:'任务完成比',
-        key:"finishedRadio",
-        content:()=>(
+        theme: 'blue',
+        title: '任务完成比',
+        key: "finishedRadio",
+        content: () => (
           <div id="task-chart" className="chart-container"></div>
         ),
       },
       {
-        theme:'yibiaopan',
-        title:'任务完成进度',
-        key:"finishedProgress",
-        content:()=>(
+        theme: 'yibiaopan',
+        title: '任务完成进度',
+        key: "finishedProgress",
+        content: () => (
           <div id="task-guage-chart" className="chart-container"></div>
         ),
       },
       {
-        theme:'green',
-        title:'今日截止任务量',
-        key:"todaysRemain",
-        content:()=>(
+        theme: 'green',
+        title: '今日截止任务量',
+        key: "todaysRemain",
+        content: () => (
           <div className='statistic-card'>
             {todaysRemain}
-          {/* <Statistic title="今日截止任务量" value={todaysRemain} /> */}
+            {/* <Statistic title="今日截止任务量" value={todaysRemain} /> */}
           </div>
         ),
       },
       {
-        theme:'yijiezhi',
-        title:'已截止任务量',
-        key:"outdatedCount",
-        content:()=>(
+        theme: 'yijiezhi',
+        title: '已截止任务量',
+        key: "outdatedCount",
+        content: () => (
           <div className='statistic-card'>
             {outdatedCount}
-          {/* <Statistic title="今日截止任务量" value={todaysRemain} /> */}
+            {/* <Statistic title="今日截止任务量" value={todaysRemain} /> */}
           </div>
         ),
       },
       {
-        theme:'pinkBlue',
-        title:'总剩余任务量',
-        key:"allRemain",
-        content:()=>(
+        theme: 'pinkBlue',
+        title: '总剩余任务量',
+        key: "allRemain",
+        content: () => (
           <div className='statistic-card'>
             {allRemain}
-          {/* <Statistic title="总剩余任务量" value={allRemain} /> */}
+            {/* <Statistic title="总剩余任务量" value={allRemain} /> */}
           </div>
         ),
       },
       {
-        theme:'pinkBlueGreen',
-        title:'今日已完成任务量',
-        key:"todaysFinished",
-        content:()=>(
+        theme: 'pinkBlueGreen',
+        title: '今日已完成任务量',
+        key: "todaysFinished",
+        content: () => (
           <div className='statistic-card'>
             {todaysFinished}
             {/* <Statistic title="今日已完成任务量" value={todaysFinished} /> */}
@@ -185,27 +234,34 @@ export default function TaskStatistics(props: IProps) {
         ),
       },
       {
-        theme:'bluePink',
-        title:'累计已完成任务量',
-        key:"allFinished",
-        content:()=>(
+        theme: 'bluePink',
+        title: '累计已完成任务量',
+        key: "allFinished",
+        content: () => (
           <div className='statistic-card'>
             {allFinished}
-          {/* <Statistic title="累计已完成任务量" value={allFinished} /> */}
-        </div>
+            {/* <Statistic title="累计已完成任务量" value={allFinished} /> */}
+          </div>
         ),
       },
     ]
-    return cards.map(i=>(
-      <div key={i.key} className={`card-container card-theme_${i.theme}`}>
-      <div className='card-container_title'>{i.title}</div>
+    return cards.map(i => (
+      <div key={i.key} className={`card-container card-theme_${i.theme} 
+        ${visibleCards.includes(i.key) ? '':'card-container_hidden'}
+      `}>
+        <div className='card-container_title'>{i.title}</div>
         {i.content()}
       </div>
     ))
   }
   return (
     <div className="card-containers">
-      <div></div>
+      <div className='card-tool-bar'>
+        <Popover placement="topLeft" content={menu} title="可见卡片配置" trigger="click" >
+          <div className='filter-icon'><FilterIcon active={false} /></div>
+        </Popover>
+
+      </div>
       <GridLayout
         className="layout"
         layout={layout}
